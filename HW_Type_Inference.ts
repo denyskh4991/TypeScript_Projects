e0e2ab2863bc74f0ae9a9c5da80846ed6cdb89d6
denyskh4991 <denyskh4991@gmail.com>
2024-10-30 21:57:45 +0100

    Adding second entity

--- HW_Type_Inference.ts
+++ HW_Type_Inference.ts
@@ -60,3 +60,35 @@ class MovieList {
         return this.filters;
     }
 }
+
+// Друга сутність - список категорій
+interface Category {
+    name: string;
+    movies: Movie[];
+}
+
+class CategoryList {
+    private categories: Category[];
+    private filters: {
+        name: GridFilterValue<string>[];
+    };
+
+    constructor(categories: Category[]) {
+        this.categories = categories;
+        this.filters = {
+            name: []
+        };
+    }
+
+    applySearchValue(value: string): void {
+        this.filters.name = [{ type: GridFilterTypeEnum.MATCH, filter: value }];
+    }
+
+    applyFiltersValue(filter: GridFilterValue<string>): void {
+        this.filters.name.push(filter);
+    }
+
+    getFilters(): typeof this.filters {
+        return this.filters;
+    }
+}
